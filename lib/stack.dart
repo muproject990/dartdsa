02cc4d97d771aea6bedcb564b584c0e342530e0d
muproject <muproject990@gmail.com>
2023-02-22 20:12:45 +0545

    challenge complete

--- lib/stack.dart
+++ lib/stack.dart
@@ -1,18 +1,24 @@
 class Stack<E> {
-  final List<E> _storage;
+  Stack() : _storage = <E>[];
+
   Stack.of(Iterable<E> elements) : _storage = List<E>.of(elements);
+
+  final List<E> _storage;
+
+  void push(E element) => _storage.add(element);
+
+  E pop() => _storage.removeLast();
+
+  E get peek => _storage.last;
+
+  bool get isEmpty => _storage.isEmpty;
+
+  bool get isNotEmpty => !isEmpty;
 
   @override
   String toString() {
-    return '---Top---\n'
-        // '${_storage.reversed}'  op -> (4,3,2,1)
+    return '--- Top ---\n'
         '${_storage.reversed.join('\n')}'
-        '\n----------------';
+        '\n-----------';
   }
-
-  void push(E element) => _storage.add(element);
-  E pop() => _storage.removeLast();
-  E get peek => _storage.last;
-  bool get isEmpty => _storage.isEmpty;
-  bool get isNotEmpty => !isEmpty;
 }
