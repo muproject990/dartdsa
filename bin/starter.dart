f4cd7646b5173e9a4580024cd2fb726ad8416b75
muproject <muproject990@gmail.com>
2023-02-22 08:11:07 +0545

    stack challenge 1 Reverse a list

--- bin/starter.dart
+++ bin/starter.dart
@@ -1,12 +1,12 @@
 import 'package:starter/stack.dart';
 
 void main() {
-const list=['s','m','o','k','e'];
-final smokeStack=Stack.of(list);
-print(smokeStack);
-smokeStack.pop();
-print(smokeStack.peek);
-
-
-
+  const list = ['s', 'm', 'o', 'k', 'e'];
+  final smokeStack = Stack.of(list);
+// ?Reversing string
+  final c = <String>[];
+  for (var i = 0; i < list.length; i++) {
+    c.insert(i, smokeStack.pop());
+  }
+  print(c);
 }
