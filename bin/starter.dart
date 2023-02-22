2066550fba35cffaa3dc7b79dbb5028c1961f296
muproject <muproject990@gmail.com>
2023-02-22 19:45:26 +0545

    c1 completed

--- bin/starter.dart
+++ bin/starter.dart
@@ -1,9 +1,10 @@
 import 'package:starter/stack.dart';
 
 void main() {
+  // reverse of string inplementations
   const list = ['s', 'm', 'o', 'k', 'e'];
   final smokeStack = Stack.of(list);
-// ?Reversing string
+
   final c = <String>[];
   for (var i = 0; i < list.length; i++) {
     c.insert(i, smokeStack.pop());
