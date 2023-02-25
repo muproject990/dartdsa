969dca1802528d09d1abf010ba92cf75a63c810a
muproject <muproject990@gmail.com>
2023-02-25 19:51:34 +0545

    complete

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,11 @@
 import 'package:starter/linked_list.dart';
 
 void main(List<String> args) {
-
+  final list = LinkedList<int>();
+  list.push(1);
+  list.push(2);
+  list.push(3);
+  list.push(4);
+  // list.append(99);
+  print(list);
 }
--- lib/linked_list.dart
+++ lib/linked_list.dart
@@ -1,15 +1,40 @@
-import 'dart:html';
-class Node<T>{
-  Node({required this.value,this.next});
+class Node<T> {
+  Node({required this.value, this.next});
   T value;
   Node<T>? next;
-}
 
+  @override
+  String toString() {
+    if (next == null) {
+      return '$value';
+    }
+    return '$value->${next.toString()}';
+  }
+}
 
 class LinkedList<E> {
   Node<E>? head;
   Node<E>? tail;
   bool get isEmpty => head == null;
+
+// !Push
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+   
+  }
+
+  // !Appending to the end of list
+  // ?Tail End INSERTION
+  void append(E value) {
+    if (isEmpty) {
+      push(value);
+      return;
+    }
+    tail!.next = Node(value: value);
+    tail = tail!.next;
+  
+  }
 
   @override
   String toString() {
