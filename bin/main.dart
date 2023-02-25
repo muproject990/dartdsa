94f5510abdc39d87d573d4064c8b011f7e8b4d49
muproject <muproject990@gmail.com>
2023-02-25 19:52:30 +0545

    append applied

--- bin/main.dart
+++ bin/main.dart
@@ -4,8 +4,7 @@ void main(List<String> args) {
   final list = LinkedList<int>();
   list.push(1);
   list.push(2);
-  list.push(3);
-  list.push(4);
-  // list.append(99);
+  list.append(3);
+  list.append(4);
   print(list);
 }
